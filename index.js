95ac0681aca3223129041259bb715ad962516389
Jaap Mengers <jaapm@q42.nl>
2014-04-19 12:27:33 +0200

    v0.2. Supports Spotify search and selecting artists, albums and tracks

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 
 var cli = require('cli'),
 	sonos = require('sonos'),
-	Q = require('q');
+	Q = require('q'),
+	http = require('http'),
+	_ = require('underscore'),
+	readline = require('readline');
+
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var device;
 
 
 sonos.Sonos.prototype.enqueueSpotify = function(uri){
@@ -24,12 +35,13 @@ sonos.Sonos.prototype.enqueueSpotify = function(uri){
       </u:AddURIToQueue>';
 
   return this.request(RENDERING_ENDPOINT, action, body, null, function(err, data){
-  	console.log(err, data);
+  	
   });
 }
 
 cli.parse({
-	play: ['p', 'Play whichever track is currently queued', 'string']
+	play: ['p', 'Play whichever track is currently queued', 'string'],
+	search: ['s', 'Search an artist in Spotify\'s collection', 'string' ]
 });
 
 
@@ -43,12 +55,118 @@ cli.main(function(args, options){
 	});
 
 
-	deferred.promise.then(function(device){
-		if(options.play){
-			device.enqueueSpotify(options.play);
-			device.play(function(err, data){
-				console.log(err, data);
-			});
+	deferred.promise.then(function(_device){
+
+		device = _device;
+
+		if(options.search){
+			var artistSelection = getArtists(options.search).then(selectArtist);
+
 		}
 	});
 });
+
+function selectArtist(searchResults){
+	console.log('');
+	_.each(searchResults.artists, function(artist, i){
+		console.log(i + '. ' + artist.name);
+	});
+
+	rl.question("Select an artist: ", function(answer){
+		var index = parseInt(answer);
+		var artist = searchResults.artists[index];
+
+		var result = getAlbumsForArtist(artist);
+		result.then(function(albums){
+			selectAlbum(albums.artist.albums);
+		});
+	});
+}
+
+function selectAlbum(albums){
+	console.log('');
+	_.each(albums, function(it, i){
+		console.log(i + '. ' + it.album.artist + ' - ' + it.album.name);
+	});
+
+	rl.question('Select an album: ', function(answer){
+		var index = parseInt(answer);
+		var it = albums[index];
+		var promise = getTracksForAlbum(it.album);
+		promise.then(function(tracks){
+			selectTrack(tracks.album);
+		});
+	})
+}
+
+function playTrack(track){
+	device.enqueueSpotify(track.href);
+	device.play(function(err, data){
+		process.exit(0);
+	});
+}
+
+function selectTrack(album){
+	console.log('');
+	_.each(album.tracks, function(track, i){
+		console.log(i + '. ' + album.artist + ' - ' + track.name);
+	});
+
+	rl.question("Select a track: ", function(answer){
+		var index = parseInt(answer);
+		var track = album.tracks[index];
+
+		playTrack(track);
+	});
+}
+
+function getTracksForAlbum(album){
+	var def = Q.defer();
+	var url = "http://ws.spotify.com/lookup/1/.json?extras=track&uri=" + album.href;
+
+	http.get(url, function(res){
+		handleResponse(res, def);
+	}).on('error', function(e) {
+		console.log("Got error: ", e);
+	});
+	return def.promise;	
+}
+
+function getAlbumsForArtist(artist){
+	var def = Q.defer();
+	var url = "http://ws.spotify.com/lookup/1/.json?extras=album&uri=" + artist.href;
+
+	http.get(url, function(res){
+		handleResponse(res, def);
+	}).on('error', function(e) {
+		console.log("Got error: ", e);
+	});
+	return def.promise;
+}
+
+
+function handleResponse(res, promise){
+    var body = '';
+
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    res.on('end', function() {
+        var resp = JSON.parse(body)
+        promise.resolve(resp);
+    });
+}
+
+function getArtists(query){
+	var def = Q.defer();
+
+	var url = "http://ws.spotify.com/search/1/artist.json?q=" + query;
+	http.get(url, function(res) {
+		handleResponse(res, def);
+	}).on('error', function(e) {
+		console.log("Got error: ", e);
+	});
+
+	return def.promise;
+}
